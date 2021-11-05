e2d12dff7f9b543978f39997212ee593c7568bc7
Illumaria <astankov.dmitry@gmail.com>
2021-11-05 22:46:54 +0300

    Rename preprocessing script files

--- test/run.sh
+++ test/run.sh
@@ -18,19 +18,19 @@ mv wav48_silence_trimmed/* $OUTPUT_DIR/raw/audio
 rm -rf txt wav48_silence_trimmed
 
 echo -e "\n1. Selecting only one mic per speaker"
-python src/preprocessing/01_preprocessing.py --input-dir $OUTPUT_DIR/raw/audio --output-dir $OUTPUT_DIR/processed/audio_single_mic --audio-ext flac
+python src/preprocessing/preprocessing.py --input-dir $OUTPUT_DIR/raw/audio --output-dir $OUTPUT_DIR/processed/audio_single_mic --audio-ext flac
 
 echo -e "\n2. Pausation cutting with VAD"
-python src/preprocessing/02_pausation_cutting.py --input-dir $OUTPUT_DIR/processed/audio_single_mic --output-dir $OUTPUT_DIR/processed/no_pause --target-sr 48000
+python src/preprocessing/pausation_cutting.py --input-dir $OUTPUT_DIR/processed/audio_single_mic --output-dir $OUTPUT_DIR/processed/no_pause --target-sr 48000
 
 echo -e "\n3. Resampling"
-python src/preprocessing/03_resampling.py --input-dir $OUTPUT_DIR/processed/no_pause --output-dir $OUTPUT_DIR/processed/resampled --resample-rate 22050
+python src/preprocessing/resampling.py --input-dir $OUTPUT_DIR/processed/no_pause --output-dir $OUTPUT_DIR/processed/resampled --resample-rate 22050
 
 echo -e "\n4. Audio to Mel"
-python src/preprocessing/04_wav_to_mel.py --input-dir $OUTPUT_DIR/processed/resampled --output-dir $OUTPUT_DIR/processed/mels
+python src/preprocessing/wav_to_mel.py --input-dir $OUTPUT_DIR/processed/resampled --output-dir $OUTPUT_DIR/processed/mels
 
 echo -e "\n5. Text normalization"
-python src/preprocessing/05_text_normalization.py --input-dir $OUTPUT_DIR/raw/text --output-dir $OUTPUT_DIR/processed/mfa_inputs
+python src/preprocessing/text_normalization.py --input-dir $OUTPUT_DIR/raw/text --output-dir $OUTPUT_DIR/processed/mfa_inputs
 
 echo -e "\n6. MFA Alignment setup"
 mfa thirdparty download
@@ -45,7 +45,7 @@ conda deactivate
 conda activate emotts
 
 echo -e "\n7. MFA Preprocessing"
-python src/preprocessing/06_mfa_preprocessing.py --input-dir $OUTPUT_DIR/processed/resampled --output-dir $OUTPUT_DIR/processed/mfa_inputs
+python src/preprocessing/mfa_preprocessing.py --input-dir $OUTPUT_DIR/processed/resampled --output-dir $OUTPUT_DIR/processed/mfa_inputs
 
 # FINALLY, align phonemes and speech
 echo -e "\n8. MFA Alignment"
@@ -55,5 +55,5 @@ mfa align -t ./temp --clean -j 4 $OUTPUT_DIR/processed/mfa_inputs models/librisp
 rm -rf temp
 
 echo -e "\n9. MFA Postprocessing"
-# python src/preprocessing/07_mfa_postprocessing.py --input-dir $OUTPUT_DIR/processed/mfa_outputs
-python src/preprocessing/07_mfa_postprocessing.py --input-dir $OUTPUT_DIR/processed/mels
+# python src/preprocessing/mfa_postprocessing.py --input-dir $OUTPUT_DIR/processed/mfa_outputs
+python src/preprocessing/mfa_postprocessing.py --input-dir $OUTPUT_DIR/processed/mels
