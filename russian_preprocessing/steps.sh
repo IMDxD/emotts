0a1d739891786046a189bbe57fb00ed1f5cf9b22
ruk0sh <43880105+ruk0sh@users.noreply.github.com>
2021-11-19 15:08:48 +0300

    finished preprocessing steps over russian dataset

--- russian_preprocessing/steps.sh
+++ russian_preprocessing/steps.sh
@@ -27,31 +27,38 @@ python src/preprocessing/wav_to_mel.py --input-dir $OUTPUT_DIR/audio/resampled -
 echo -e "\n5) Text normalization"
 python src/preprocessing/text_normalization_russian.py --input-dir $OUTPUT_DIR/text/raw --output-dir $OUTPUT_DIR/mfa_inputs
 
-echo -e "\n6. MFA Alignment setup"
+echo -e "\n6) MFA Alignment setup"
 
-# download a pretrained russian acoustic model, and russian lexicon
+# download a pretrained russian acoustic model, and russian g2p model
 mkdir -p models
-wget -q --show-progress http://mlmlab.org/mfa/mfa-models/russian.zip -P models/mfa
-wget -q --show-progress http://mlmlab.org/mfa/mfa-models/g2p/russian_g2p.zip -P models/g2p
-wget -q --show-progress http://www.openslr.org/resources/11/librispeech-lexicon.txt -P models/mfa
+wget -q --show-progress https://github.com/MontrealCorpusTools/mfa-models/raw/main/acoustic/russian.zip -P models/mfa
+wget -q --show-progress https://github.com/MontrealCorpusTools/mfa-models/raw/main/g2p/russian_g2p.zip -P models/g2p
 
 conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib  # link to libopenblas
 conda deactivate
 conda activate emotts
 
 # 16069/16069 [00:00<00:00, 21508.15it/s]
-python src/preprocessing/create_corpus.py --input-dir $OUTPUT_DIR/text/raw --output-path $OUTPUT_DIR/meta/lexicon.txt
+echo -e "\n6.1) Creating word list from dataset"
+python src/preprocessing/create_corpus.py --input-dir $OUTPUT_DIR/text/raw --output-path $OUTPUT_DIR/meta/words.txt
 
+# 25849/25983 [01:19<00:00, 326.82it/s]
+echo -e "\n6.2) Creating G2P lexicon from word list"
+mfa g2p -t mfa_tmp -j 32 --clean --overwrite models/g2p/russian_g2p.zip $OUTPUT_DIR/meta/words.txt models/mfa
+rm -rf mfa_tmp
+
+# seconds
 echo -e "\n7. MFA Preprocessing"
-python src/preprocessing/mfa_preprocessing.py --input-dir $OUTPUT_DIR/processed/resampled --output-dir $OUTPUT_DIR/processed/mfa_inputs
+python src/preprocessing/mfa_preprocessing.py --input-dir $OUTPUT_DIR/audio/resampled --output-dir $OUTPUT_DIR/mfa_inputs
 
 # FINALLY, align phonemes and speech
+# ~15 min
 echo -e "\n8. MFA Alignment"
 echo $OUTPUT_DIR
-
-mfa align -t ./temp --clean -j 4 $OUTPUT_DIR/processed/mfa_inputs models/librispeech-lexicon.txt models/english.zip $OUTPUT_DIR/processed/mfa_outputs
-rm -rf temp
+mfa align -t mfa_tmp --clean -j 32 $OUTPUT_DIR/mfa_inputs models/mfa/russian_lexicon.txt models/mfa/russian.zip $OUTPUT_DIR/mfa_outputs
+rm -rf mfa_tmp
 
 echo -e "\n9. MFA Postprocessing"
 # Aggregate mels by speakers
-python src/preprocessing/mfa_postprocessing.py --input-dir $OUTPUT_DIR/processed/mels
+# 16071it [00:00, 16767.84it/s]
+python src/preprocessing/mfa_postprocessing.py --input-dir $OUTPUT_DIR/mels
