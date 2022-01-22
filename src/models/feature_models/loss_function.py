10ac469494120606a4bc19a1e4f11b1d557e604b
IMDxD <nestor969@mail.ru>
2022-01-22 13:20:10 +0300

    add early stopping

--- src/models/feature_models/loss_function.py
+++ src/models/feature_models/loss_function.py
@@ -3,17 +3,21 @@ from typing import Tuple
 import torch
 import torch.nn.functional as F
 from torch import nn
+from src.train_config import LossParams
 
 
 class NonAttentiveTacotronLoss(nn.Module):
     def __init__(
         self,
         sample_rate: int,
-        hop_size: int
+        hop_size: int,
+        config: LossParams
     ):
         super(NonAttentiveTacotronLoss, self).__init__()
         self.sample_rate = sample_rate
         self.hop_size = hop_size
+        self.mel_weight = config.mels_weight
+        self.duration_weight = config.duration_weight
 
     def forward(
         self, prenet_mels: torch.Tensor, postnet_mels: torch.Tensor, model_durations: torch.Tensor,
@@ -26,9 +30,9 @@ class NonAttentiveTacotronLoss(nn.Module):
         prenet_l2 = F.mse_loss(prenet_mels, target_mels)
         postnet_l1 = F.l1_loss(postnet_mels, target_mels)
         postnet_l2 = F.mse_loss(postnet_mels, target_mels)
-        loss_prenet = prenet_l1 + prenet_l2
-        loss_postnet = postnet_l1 + postnet_l2
+        loss_prenet = self.mel_weight * (prenet_l1 + prenet_l2)
+        loss_postnet = self.mel_weight * (postnet_l1 + postnet_l2)
         model_durations = model_durations * self.hop_size / self.sample_rate
         target_durations = target_durations * self.hop_size / self.sample_rate
-        loss_durations = F.mse_loss(model_durations, target_durations)
+        loss_durations = self.duration_weight * F.mse_loss(model_durations, target_durations)
         return loss_prenet, loss_postnet, loss_durations
