e1367a72d073446500f4a8da47a8fde82aa7e85e
IMDxD <nestor969@mail.ru>
2022-01-29 13:00:39 +0300

    fix bug with zero ranges in distribution

--- src/models/feature_models/non_attentive_tacotron.py
+++ src/models/feature_models/non_attentive_tacotron.py
@@ -175,8 +175,8 @@ class Attention(nn.Module):
         duration_cumsum = durations.cumsum(dim=1).float()
         max_duration = duration_cumsum[:, -1, :].max().long()
         mu = duration_cumsum - 0.5 * durations
-        distr = Normal(mu, ranges)
         ranges = torch.maximum(ranges, self.EPS.to(ranges.device))
+        distr = Normal(mu, ranges)
         t = torch.arange(0, max_duration.item()).view(1, 1, -1).to(ranges.device)
 
         weights: torch.Tensor = torch.exp(distr.log_prob(t))
