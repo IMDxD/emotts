1c39ed71529bddd22d4eb789ff6547317d9a8dbc
IMDxD <nestor969@mail.ru>
2022-03-31 20:58:40 +0300

    add last 20 speaker

--- src/data_process/config.py
+++ src/data_process/config.py
@@ -13,5 +13,5 @@ class VCTKDatasetParams:
     text_ext: str = field(default=".TextGrid")
     mels_ext: str = field(default=".pkl")
     finetune_speakers: List[str] = field(
-        default_factory=lambda: [f"00{i}" for i in range(11, 20)]
+        default_factory=lambda: [f"00{i}" for i in range(11, 21)]
     )
