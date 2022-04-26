6db3328ef01c7adc0179195ed751caebe1e3fc7d
IMDxD <nestor969@mail.ru>
2022-04-26 19:16:45 +0300

    fix data split in hifi

--- src/models/hifi_gan/train_valid_split.py
+++ src/models/hifi_gan/train_valid_split.py
@@ -1,50 +1,37 @@
-from pathlib import Path, PurePath
+import random
+from pathlib import Path
+from typing import List
 
-import numpy as np
-
-NON_VALID_SPEAKERS = ["p315"]
-VALID_SIZE = 9
-SPLIT_LOG_PATH = Path("logs/hifi-split.txt")
+from src.data_process.config import VCTKDatasetParams
 
 
-def get_mel_file_path(full_wav_name: str, mels_dir: str, suffix: str=".pth"):
-    return Path(
-            mels_dir,
-            Path(*list(Path(full_wav_name).parts[-2:])).with_suffix(suffix)
-            )
+def split_vctk_data(data_config: VCTKDatasetParams, test_size: float):
+    wavs_dir_path = Path(data_config.wav_dir)
+    mels_dir_path = Path(data_config.mels_dir)
 
+    texts_set = {
+        x.stem for x in wavs_dir_path.rglob("*wav")
+    }
+    mels_set = {
+        x.stem for x in mels_dir_path.rglob(f"*{data_config.mels_ext}")
+    }
+    samples = list(mels_set & texts_set)
 
-def split_vctk_data(wavs_dir: str, mels_dir: str):
-    wavs_dir_path = Path(wavs_dir)
-    speaker_ids = []
-    esd_speakers = [folder.name for folder in wavs_dir_path.iterdir() if folder.name.startswith("00")]
-    for folder in wavs_dir_path.iterdir():
-        if folder.is_dir() and folder.name not in NON_VALID_SPEAKERS and not folder.name.startswith("00"):
-            speaker_ids.append(folder.name)
+    speakers_to_data_id = {speaker: list(speaker.iterdir()) for speaker in mels_dir_path.iterdir()}
+    test_ids: List[int] = []
+    for speaker, ids in speakers_to_data_id.items():
+        test_size = int(len(ids) * test_size)
+        if test_size > 0 and speaker not in data_config.ignore_speakers:
+            test_indexes = [file.stem for file in random.choices(ids, k=test_size)]
+            test_ids.extend(test_indexes)
 
-    np.random.shuffle(speaker_ids)
-    train_ids, valid_ids = speaker_ids[:-VALID_SIZE], speaker_ids[-VALID_SIZE:]
-    train_ids.extend(esd_speakers)
-    print(f"Train speakers: {train_ids}")
-    print(f"Valid speakers: {valid_ids}")
-    training_files, validation_files = [], []
-    for folder in wavs_dir_path.iterdir():
-        if folder.name in speaker_ids:
-            for file in folder.iterdir():
-                full_wav_name = PurePath(file).as_posix()
-                mels_file_path = get_mel_file_path(full_wav_name, mels_dir)
-                if mels_file_path.is_file():
-                    if folder.name in train_ids:
-                        training_files.append(full_wav_name)
-                    else:
-                        validation_files.append(full_wav_name)
+    train_data = []
+    test_data = []
+    for file in wavs_dir_path.rglob("*wav"):
+        if file.stem in samples:
+            if file.stem in test_ids:
+                test_data.append(file.as_posix())
+            else:
+                train_data.append(file.as_posix())
 
-    # Save  train and val filenames for sanity check
-    Path(*list(SPLIT_LOG_PATH.parts[:-1])).mkdir(parents=True, exist_ok=True)
-    with open(SPLIT_LOG_PATH, "w") as log_file:
-        log_file.write("train:")
-        log_file.writelines(training_files)
-        log_file.write("val:")
-        log_file.writelines(validation_files)
-
-    return training_files, validation_files
+    return train_data, test_data
