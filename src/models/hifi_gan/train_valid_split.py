9d00d4024f77eb879e9957578fdf7ab825f35222
IMDxD <nestor969@mail.ru>
2022-04-27 09:54:09 +0300

    fix files split

--- src/models/hifi_gan/train_valid_split.py
+++ src/models/hifi_gan/train_valid_split.py
@@ -5,31 +5,28 @@ from typing import List
 from src.data_process.config import VCTKDatasetParams
 
 
-def get_mel_file_path(full_wav_name: str, mels_dir: str, suffix: str=".pth"):
+def get_mel_file_path(full_wav_name: str, mels_dir: str, suffix: str = ".pth"):
     return Path(
-            mels_dir,
-            Path(*list(Path(full_wav_name).parts[-2:])).with_suffix(suffix)
-            )
+        mels_dir, Path(*list(Path(full_wav_name).parts[-2:])).with_suffix(suffix)
+    )
 
 
-def split_vctk_data(data_config: VCTKDatasetParams, test_size: float):
+def split_vctk_data(data_config: VCTKDatasetParams, test_fraction: float):
     wavs_dir_path = Path(data_config.wav_dir)
-    mels_dir_path = Path(data_config.mels_dir)
+    mels_dir_path = Path(data_config.feature_dir)
 
-    texts_set = {
-        x.stem for x in wavs_dir_path.rglob("*wav")
-    }
-    mels_set = {
-        x.stem for x in mels_dir_path.rglob(f"*{data_config.mels_ext}")
-    }
-    samples = list(mels_set & texts_set)
+    audio_set = {x.stem for x in wavs_dir_path.rglob("*wav")}
+    mels_set = {x.stem for x in mels_dir_path.rglob("*pth")}
+    samples = list(mels_set & audio_set)
 
-    speakers_to_data_id = {speaker: list(speaker.iterdir()) for speaker in mels_dir_path.iterdir()}
+    speakers_to_data_id = {
+        speaker.name: list(speaker.iterdir()) for speaker in mels_dir_path.iterdir()
+    }
     test_ids: List[int] = []
     for speaker, ids in speakers_to_data_id.items():
-        test_size = int(len(ids) * test_size)
-        if test_size > 0 and speaker not in data_config.ignore_speakers:
-            test_indexes = [file.stem for file in random.choices(ids, k=test_size)]
+        test_size = int(len(ids) * test_fraction)
+        if test_size > 0 and speaker in data_config.ignore_speakers:
+            test_indexes = random.choices([file.stem for file in ids], k=test_size)
             test_ids.extend(test_indexes)
 
     train_data = []
