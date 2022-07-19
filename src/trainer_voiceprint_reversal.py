3756c38e9374afc1d91aca4b61c2ca2264aad158
IMDxD <nestor969@mail.ru>
2022-07-19 17:22:26 +0300

    fix reference number of dimensions

--- src/trainer_voiceprint_reversal.py
+++ src/trainer_voiceprint_reversal.py
@@ -445,6 +445,7 @@ class Trainer:
                     reference = (
                         torch.load(reference_path, map_location="cpu") - self.mels_mean
                     ) / self.mels_std
+                    reference = reference.unsqueeze(0)
                     batch = (
                         phonemes_tensor,
                         num_phonemes_tensor,
